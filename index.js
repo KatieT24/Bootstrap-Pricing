7cc5ed2d69cd236d30c117a0ff7bfbad2bf5eb75
KatieT24 <whitedove-14@hotmail.com>
2024-03-24 17:43:42 -0600

    I made some code Changes!

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hey what is up?!?!?!");
+
+let yourName = "Katrina";
+console.log("This is my name: " + yourName);
+
+// let coolnavdata = document.getElementById("specialnav");
+// console.log("This is my nav data???", coolnavdata);
+// coolnavdata.innerHTML = "<nav>This was your old nav until I changed it</nav>";
